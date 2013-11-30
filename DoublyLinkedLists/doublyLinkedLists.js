bb6e67eaa904dc8bb941090f16ba082b12bb79f5
Alejandro Lechuga <alejandrolechuga@Alejandros-MacBook-Pro-2.local>
2013-11-30 10:25:59 -0800

    new methods for adding nodes at DoublyLinkedList

--- DoublyLinkedLists/doublyLinkedLists.js
+++ DoublyLinkedLists/doublyLinkedLists.js
@@ -4,7 +4,31 @@ function Node(value) {
 	this.value = value;
 }
 
-function DoubleLinkedList () {
+function DoubleLinkedList() {
 	this.first;
-	this.tail;
+	this.last;
 }
+
+DoubleLinkedList.prototype.addAtBeginning = function (node) {
+	var hold;
+	if (this.first) {
+		hold = this.first;
+		this.first = node;
+		hold.previous = this.first;
+		this.first.next = hold;
+	} else {
+		this.first = node;
+	}
+};
+
+DoubleLinkedList.prototype.addAtEnd = function(node) {
+	var hold;
+	if (this.last) {
+		hold = this.last;
+		this.last = node;
+		hold.next = this.last;
+		this.last.previous = hold;
+	} else {
+		this.last = node;
+	}
+};

d15293aa300ca804b7354aff64e90b744a97bf19
Alejandro Lechuga <ing.ramon.alejandro@gmail.com>
2014-02-08 00:27:20 -0800

    Adding the lookup method for Binary Search Tree

--- BinaryTree/binaryTree.js
+++ BinaryTree/binaryTree.js
@@ -8,6 +8,22 @@ function BinaryTree () {
   this.root;
 }
 
+BinaryTree.prototype.lookup = function (node, target) {
+  // Base case == emty tree
+  // if the target is not found return a false value
+  if (!node) return false;
+  else {
+    // check if is found here 
+    if (target == node.data) return node;
+    else {
+      //otherwise recur down to the correct subtree 
+      // choose the side to base on the less-than operation
+      if (target < node.data) return this.lookup(node.left, target);
+      else return this.lookup(node.right,target);
+    }
+  }
+};
+
 BinaryTree.prototype.insert =  function () {
-  
+
 };

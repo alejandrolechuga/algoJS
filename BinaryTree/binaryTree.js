48bf5ea06424b7c765dddd40e478aa065ce80dde
Alejandro Lechuga <ing.ramon.alejandro@gmail.com>
2014-02-09 15:18:23 -0800

    insert method implemented

--- BinaryTree/binaryTree.js
+++ BinaryTree/binaryTree.js
@@ -1,5 +1,5 @@
 function Node (data) {
-  this.value = data;
+  this.data = data;
   this.right = null;
   this.left = null;
 }
@@ -11,19 +11,37 @@ function BinaryTree () {
 BinaryTree.prototype.lookup = function (node, target) {
   // Base case == emty tree
   // if the target is not found return a false value
-  if (!node) return false;
+  if (!node) { 
+    return false;
+  }
   else {
     // check if is found here 
-    if (target == node.data) return node;
+    if (target == node.data) {
+      return node;
+    }
     else {
       //otherwise recur down to the correct subtree 
       // choose the side to base on the less-than operation
-      if (target < node.data) return this.lookup(node.left, target);
-      else return this.lookup(node.right,target);
+      if (target < node.data) {
+        return this.lookup(node.left, target);
+      } else  {
+        return this.lookup(node.right, target);
+      }
     }
   }
 };
 
-BinaryTree.prototype.insert =  function () {
-
+BinaryTree.prototype.insert =  function (node, data) {
+  // if the node is null return a new node
+  if (node == null) {
+    return Node(data);
+  } else {
+    // otherwise recur down the tree
+    if (node.data <= data) {
+      node.left = this.insert(node.left , data);
+    } else {
+      node.right = this.insert(node.right, data);
+    }
+    return node;
+  }
 };

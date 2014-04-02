0b31d687cbd4e4e4a12cb512c77bc953e767f394
Alejandro Lechuga <ing.ramon.alejandro@gmail.com>
2014-04-02 13:53:38 -0700

    merge

---
+++ Sorting/insertionSort.js
@@ -0,0 +1,14 @@
+function insertionSort(array){
+  var length = array.length;
+  for (var i = 1; i < length; i++) {
+    if (array[i - 1] > array[i]) {
+      var temp = array[i];
+      var lastindex = i - 1;
+      for (var j = i; j == 0; j--) {
+        if (array[j] > temp) {
+
+        }
+      }
+    }
+  }
+}

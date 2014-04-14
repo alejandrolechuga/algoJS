f11f5092f80c43eafdc8072589e448ec5cc1cfc6
Alejandro Lechuga <ing.ramon.alejandro@gmail.com>
2014-04-13 21:06:33 -0700

    update bag implementation

--- Data Structures/Bag.js
+++ Data Structures/Bag.js
@@ -1,15 +1,26 @@
 function Bag() {
-
+  this.size = 100;
+  this.length = 0;
+  this.head = null;
+  this.Node = function () {
+    this.item;
+    this.next;
+  }
 }
 
 Bag.prototype.add = function(item) {
-
+  var head = this.head;
+  var node = new this.Node();
+  node.item = item;
+  this.head = node;
+  node.next = head;
+  this.length++;
 };
 
 Bag.prototype.isEmpty = function () {
-
+  return this.head === null;
 };
 
 Bag.prototype.size = function () {
-
+  return length;
 };
